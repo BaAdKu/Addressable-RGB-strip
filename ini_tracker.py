0a925c4c11bb4acf7c3e203d06c84faa9ed42437
Sebastian <basti.kunst@gmail.com>
2024-06-14 11:47:32 +0200

    changing way i increment

--- ini_tracker.py
+++ ini_tracker.py
@@ -23,21 +23,21 @@ while True:
     pixel1.fill(0x202020)
     pixel1.show()
 
-    j=int((char_order[i]-1)*(300/char_num))
-    while j<(char_order[i]*(300/char_num)):
+    j=int((char_order[(i-1)%char_num])*(300/char_num))
+    while j<(char_order[i%char_num]*(300/char_num)):
         pixel1[j]=(00, 221, 00)
         j+=1
 
-    k=int((char_order[i])*(300/char_num))
-    while k<(char_order[i+1]*(300/char_num)):
+    k=int((char_order[i%char_num])*(300/char_num))
+    while k<(char_order[(i+1)%char_num]*(300/char_num)):
         pixel1[k]=(221, 221, 00)
         k+=1
 
     pixel1.show()
     i+=1
-
-    if i==char_num:
-        i=0
+    print ("this is round number " + int(i/char_num +1) + "of the initiative", end="/r")
+    # if i==char_num:
+    #     i=0
 
 pixel1.fill(0x000000)
 pixel1.show()
