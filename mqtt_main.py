681e728cd4e768bda22e0ab4b5c7c18730f1b748
Sebastian Kunst <31829042+BaAdKu@users.noreply.github.com>
2024-06-19 13:17:06 +0200

    strings

--- mqtt_main.py
+++ mqtt_main.py
@@ -11,7 +11,7 @@ with open('secrets.json', 'r') as file:
 
     def on_message(client, userdata, msg):
         print(msg.payload)
-        if "shootingstar" in msg.payload:
+        if "shootingstar" in str(msg.payload):
             shooting_star
     
     client=mqttclient.Client(mqttclient.CallbackAPIVersion.VERSION2)
