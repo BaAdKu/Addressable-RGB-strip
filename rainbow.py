1ce3c7847f4d94de394905ca6f08b0079c623765
Sebastian Kunst <31829042+BaAdKu@users.noreply.github.com>
2024-06-19 14:13:36 +0200

    ending rainbow

--- rainbow.py
+++ rainbow.py
@@ -46,12 +46,16 @@ def wheel(pos):
 
 
 def rainbow_cycle(wait):
-    for j in range(255):
-        for i in range(num_pixels):
-            pixel_index = (i * 256 // num_pixels) + j
-            pixels[i] = wheel(pixel_index & 255)
-        pixels.show()
-        time.sleep(0.001)
+    for k in range(300):
+        for j in range(255):
+            for i in range(num_pixels):
+                pixel_index = (i * 256 // num_pixels) + j
+                pixels[i] = wheel(pixel_index & 255)
+            pixels.show()
+            time.sleep(0.001)
+            k+=1
+    pixels.fill((30, 0, 80))
+    pixels.show()
 
 
 # while True:
