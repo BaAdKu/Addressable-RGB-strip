0cfbfcb89d6a13a95eee33bf6cb9655f03a541e4
Sebastian <basti.kunst@gmail.com>
2024-07-05 16:44:47 +0200

    f

--- timer.py
+++ timer.py
@@ -10,34 +10,34 @@ def timer_start( sec):
 
     timersecs = int(sec)
 
-x=timersecs/pixelnum
+    x=timersecs/pixelnum
 
-print("one led every " + str(x) + " seconds")
-
-i=0
-while i<300:
-    pixel1[i]=(255, 255, 0)
-    i+=1
-    time.sleep(x)
-i=0
-while i<pixelnum:
-    pixel1[i]=(255, 0, 0)
-    i+=1
-    time.sleep(x)
+    print("one led every " + str(x) + " seconds")
 
     i=0
-    while i<3:
-        pixel1.fill((255,0,0))
-        time.sleep(0.2)
-        pixel1.fill((0,0,0))
-        time.sleep(0.2)
+    while i<300:
+        pixel1[i]=(255, 255, 0)
         i+=1
+        time.sleep(x)
+    i=0
+    while i<pixelnum:
+        pixel1[i]=(255, 0, 0)
+        i+=1
+        time.sleep(x)
 
-i=0
-while i<pixelnum:
-    pixel1[i]=(255, 0, 0)
-    i+=1
-    time.sleep(x)
+        i=0
+        while i<3:
+            pixel1.fill((255,0,0))
+            time.sleep(0.2)
+            pixel1.fill((0,0,0))
+            time.sleep(0.2)
+            i+=1
 
-    pixel1.fill((0,0,0))
+    i=0
+    while i<pixelnum:
+        pixel1[i]=(255, 0, 0)
+        i+=1
+        time.sleep(x)
+
+        pixel1.fill((0,0,0))
 
